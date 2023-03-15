a5ac73c1397b59dd18cd1ca713b64ce8b68ff31d
Abhishikth M <54182303+aoeris@users.noreply.github.com>
2023-03-15 14:53:06 +0530

    Add files via upload

--- SELSCA_V1.0/backend/models/Grades.js
+++ SELSCA_V1.0/backend/models/Grades.js
@@ -18,21 +18,39 @@ const GradesSchema = new Schema({
     fa1 : {
         type : Number,
     },
+    fa1Date : {
+        type : Date
+    },
     fa2 : {
         type : Number,
+    },
+    fa2Date : {
+        type : Date
     },
     sa1 : {
         type : Number,
     },
+    sa1Date : {
+        type : Date
+    },
     fa3 : {
         type : Number,
+    },
+    fa3Date : {
+        type : Date
     },
     fa4 : {
         type : Number,
     },
+    fa4Date : {
+        type : Date
+    },
     sa2 : {
         type : Number,
        
+    },
+    sa2Date : {
+        type : Date
     },
     finalGrade : {
         type : String
